d888ea9a430b97bb150180e67c575e6181f32704
fhaze <eder_arm@icloud.com>
2022-02-13 16:05:54 -0300

    base64decode secret

--- secrets/index.js
+++ secrets/index.js
@@ -1,6 +1,8 @@
+
+
 module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  CLIENT_ID: process.env.CLIENT_ID,
-  GUILD_ID: process.env.GUILD_ID,
-  RAPID_API_KEY: process.env.RAPID_API_KEY,
+  BOT_TOKEN: Buffer.from(process.env.BOT_TOKEN, 'base64').toString('utf-8'),
+  CLIENT_ID: Buffer.from(process.env.CLIENT_ID, 'base64').toString('utf-8'),
+  GUILD_ID: Buffer.from(process.env.GUILD_ID, 'base64').toString('utf-8'),
+  RAPID_API_KEY: Buffer.from(process.env.RAPID_API_KEY, 'base64').toString('utf-8'),
 }
