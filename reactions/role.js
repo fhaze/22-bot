3a6c41fdf9be359e7e29a8d315d616b2cf5d7905
fhaze <eder_arm@icloud.com>
2022-02-26 04:47:38 -0300

    removed duplicated code

--- reactions/role.js
+++ reactions/role.js
@@ -1,29 +1,23 @@
+const getUserAndRoleAndExecute = (reaction, user, func) => {
+  const embed = reaction.message.embeds[0]
+  const rolesTuple = embed.description.split("\n")
+
+  for (let roleTuple of rolesTuple) {
+    const [emoji, role] = roleTuple.split(" ")
+    if (emoji.includes(reaction.emoji.id)) {
+      const r = reaction.message.guild.roles.cache.find(r => role.includes(r.id))
+      const u = reaction.message.guild.members.cache.find(m => m.id === user.id)
+      u.roles[func](r)
+    }
+  }
+}
+
 module.exports = {
   title: "Role Chooser",
   add: (reaction, user) => {
-    const embed = reaction.message.embeds[0]
-    const rolesTuple = embed.description.split("\n")
-
-    for (let roleTuple of rolesTuple) {
-      const [emoji, role] = roleTuple.split(" ")
-      if (emoji.includes(reaction.emoji.id)) {
-        const r = reaction.message.guild.roles.cache.find(r => role.includes(r.id))
-        const u = reaction.message.guild.members.cache.find(m => m.id === user.id)
-        u.roles.add(r)
-      }
-    }
+    getUserAndRoleAndExecute(reaction, user, "add")
   },
   remove: (reaction, user) => {
-    const embed = reaction.message.embeds[0]
-    const rolesTuple = embed.description.split("\n")
-
-    for (let roleTuple of rolesTuple) {
-      const [emoji, role] = roleTuple.split(" ")
-      if (emoji.includes(reaction.emoji.id)) {
-        const r = reaction.message.guild.roles.cache.find(r => role.includes(r.id))
-        const u = reaction.message.guild.members.cache.find(m => m.id === user.id)
-        u.roles.remove(r)
-      }
-    }
+    getUserAndRoleAndExecute(reaction, user, "remove")
   }
 }
