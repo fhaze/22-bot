6934d232f49d03d1fc93d6ea5c16cf055b8e1102
fhaze <eder_arm@icloud.com>
2022-02-27 18:41:41 -0300

    remove timestamp from log

--- logger/index.js
+++ logger/index.js
@@ -2,7 +2,7 @@ const { createLogger, format, transports } = require('winston')
 
 const logger = createLogger({
   level: 'info',
-  format: format.combine(format.timestamp(), format.json()),
+  format: format.json(),
   transports: [
     new transports.Console()
   ]
